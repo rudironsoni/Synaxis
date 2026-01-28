ced497cf20e239f19af0f55ecb026ec3236fb89c
Rudimar Ronsoni <rudimar@outlook.com>
2026-01-28 11:39:45 +0100

    tests: pass AntigravitySettings into AntigravityAuthManager in unit tests; require settings in manager and fix BuildAuthorizationUrl instance method

---
+++ src/InferenceGateway/Application/Configuration/AntigravitySettings.cs
@@ -0,0 +1,7 @@
+namespace Synaxis.InferenceGateway.Application.Configuration;
+
+public class AntigravitySettings
+{
+    public string ClientId { get; set; } = string.Empty;
+    public string ClientSecret { get; set; } = string.Empty;
+}
